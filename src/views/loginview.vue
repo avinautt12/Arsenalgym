02263ba9bd4d8cc3d9329cac762565f2ad40b6aa
JesusAlbertoTovar <tovar25072005@gmail.com>
2024-08-21 17:05:23 -0600

    login

--- src/views/loginview.vue
+++ src/views/loginview.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-app>
     <v-layout class="rounded rounded-md" style="min-height: 100vh; background-color: #f5f5f5;">
@@ -7,60 +6,32 @@
       <v-main>
         <v-container>
           <v-row>
-            <!-- Formulario para clientes -->
-            <v-col cols="12" md="6">
+            <!-- Formulario combinado para clientes y socios -->
+            <v-col cols="12" md="6" class="mx-auto">
               <v-card class="pa-5 card-form card-color" max-width="500">
                 <v-card-title class="titulo-formulario">
                   Iniciar Sesión
                 </v-card-title>
                 <v-card-text>
-                  <v-form ref="form1">
+                  <v-form ref="form">
                     <v-text-field
-                      v-model="correo"
-                      label="Correo electrónico"
-                      type="email"
-                      :rules="[v => !!v || 'Correo es requerido', v => /.+@.+/.test(v) || 'Correo debe ser válido']"
+                      v-model="identificador"
+                      label="Correo electrónico o Usuario"
+                      :rules="[v => !!v || 'Este campo es requerido']"
                     ></v-text-field>
                     <v-text-field
-                      v-model="contrasena1"
+                      v-model="contrasena"
                       label="Contraseña"
                       type="password"
                       :rules="[v => !!v || 'Contraseña es requerida']"
                     ></v-text-field>
-                    <v-btn class="boton-ingresar" @click="ingresarFormulario1">Ingresar</v-btn>
+                    <v-btn class="boton-ingresar" @click="ingresarFormulario">Ingresar</v-btn>
                   </v-form>
                 </v-card-text>
                 <v-card-actions>
                   <v-spacer></v-spacer>
                   <router-link to="/Registro" class="link">¿No estás registrado?</router-link>
                 </v-card-actions>
-              </v-card>
-            </v-col>
-
-            <!-- Formulario para socios -->
-            <v-col cols="12" md="6">
-              <v-card class="pa-5 card-form card-color" max-width="500">
-                <v-card-title class="titulo-formulario2">
-                  Team Arsenal
-                </v-card-title>
-                <v-card-text>
-                  <v-form ref="form2">
-                    <v-text-field
-                      v-model="usuario"
-                      label="Usuario"
-                      :rules="[v => !!v || 'Usuario es requerido']"
-                      class="campo-input-derecha"
-                    ></v-text-field>
-                    <v-text-field
-                      v-model="contrasena2"
-                      label="Contraseña"
-                      type="password"
-                      :rules="[v => !!v || 'Contraseña es requerida']"
-                      class="campo-input-derecha"
-                    ></v-text-field>
-                    <v-btn class="boton-ingresar" @click="ingresarFormulario2">Ingresar</v-btn>
-                  </v-form>
-                </v-card-text>
               </v-card>
             </v-col>
           </v-row>
@@ -76,38 +47,37 @@ import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/userStore';
 import barraNav from '@/components/barraNav.vue';
 
-const correo = ref('');
-const contrasena1 = ref('');
-const usuario = ref('');
-const contrasena2 = ref('');
+const identificador = ref('');
+const contrasena = ref('');
 const router = useRouter();
 const userStore = useUserStore();
 
-const ingresarFormulario1 = async () => {
+const ingresarFormulario = async () => {
+  if (!identificador.value || !contrasena.value) {
+    alert('Por favor, llena todos los campos.');
+    return;
+  }
+
   try {
-    const response = await fetch('http://mipagina.com/loginClientes', {
+    const response = await fetch('http://mipagina.com/login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        correo: correo.value,
-        contrasena: contrasena1.value
-      })
+        identificador: identificador.value,
+        contrasena: contrasena.value,
+      }),
     });
-
-    if (!response.ok) {
-      throw new Error('Error en la respuesta de red');
-    }
 
     const result = await response.json();
 
-    if (result.status === 200) {
+    if (response.ok && result.status === 200) {
       localStorage.setItem('token', result.data._token);
       userStore.setUsuario(result.data.usuario);
-      router.push({ name: 'perfilusuario' });
+      router.push({ name: result.data.usuario.tipoUsuario === 'socio' ? 'info' : 'perfilusuario' });
     } else {
-      alert(result.data.msg || 'Credenciales inválidas.');
+      alert(result.msg || 'Credenciales inválidas.');
     }
   } catch (error) {
     console.error('Error en la solicitud:', error);
@@ -115,64 +85,24 @@ const ingresarFormulario1 = async () => {
   }
 };
 
-const ingresarFormulario2 = async () => {
-  if (usuario.value === 'Peniche1234' && contrasena2.value === '123456') {
-    router.push({ name: 'AdminInicio' });
-  } else {
-    try {
-      const response = await fetch('http://mipagina.com/loginSocios', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          usuario: usuario.value,
-          contrasena: contrasena2.value
-        })
-      });
 
-      if (!response.ok) {
-        throw new Error('Error en la respuesta de red');
-      }
 
-      const result = await response.json();
-
-      if (result.status === 200 && result.msg === 'success') {
-        localStorage.setItem('token', result.data._token);
-        userStore.setUsuario(result.data.usuario);
-        router.push({ name: 'info' });
-      } else {
-        alert(result.msg || 'Credenciales inválidas.');
-      }
-    } catch (error) {
-      console.error('Error en la solicitud:', error);
-      alert('Hubo un error al intentar iniciar sesión. Por favor, inténtelo de nuevo.');
-    }
-  }
-};
 
 </script>
 
 <style scoped>
 .v-main {
-  background:linear-gradient(black,red); /* Color de fondo sólido */
+  background: linear-gradient(black, red); /* Color de fondo sólido */
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.titulo-formulario, .titulo-formulario2 {
+.titulo-formulario {
   text-align: center;
   margin-bottom: 20px;
   font-size: 24px;
   font-weight: bold;
-}
-
-.titulo-formulario {
-  color: #333;
-}
-
-.titulo-formulario2 {
   color: #333;
 }
 
@@ -183,7 +113,7 @@ const ingresarFormulario2 = async () => {
 }
 
 .card-color {
-  background-color: #fff; /* Fondo blanco para ambos formularios */
+  background-color: #fff; /* Fondo blanco */
 }
 
 .boton-ingresar {
